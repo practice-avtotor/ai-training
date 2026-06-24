c97b39aaed762c7465bd2ebfafab5903b84f252f
RedWolf77 <romario270704@mail.ru>
2026-06-24 10:48:31 +0200

    refactor: separate function for loading model for CPT

--- src/model.py
+++ src/model.py
@@ -1,30 +1,19 @@
 import torch
 from transformers import AutoModelForCausalLM, BitsAndBytesConfig
-from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training, PeftModel
 from ..config import MODEL_NAME, LORA_R, LORA_ALPHA, LORA_DROPOUT
 
 # Предполагается использовать видеокарту RTX 3080ti 12GB
 
-def load_model():
+def load_base_quantized_model():
     """
-    Загрузка модели в память видеокарты
+    Загружает только чистую базовую модель в 4-бит
     """
-
     # Сжатие модели в 4 бита
     bnb_config = BitsAndBytesConfig(
         load_in_4bit=True,
         bnb_4bit_quant_type="nf4",
         bnb_4bit_compute_dtype=torch.bfloat16
-    )
-
-    # Настройка конфига LoRa
-    lora_config = LoraConfig(
-        r=LORA_R,
-        lora_alpha=LORA_ALPHA,
-        lora_dropout=LORA_DROPOUT,
-        bias="none",
-        task_type="CAUSAL_LM",
-        target_modules="all-linear"
     )
 
     # Загрузка модели
@@ -37,9 +26,24 @@ def load_model():
 
     # Подготавливаем модель для kbit обучения
     model = prepare_model_for_kbit_training(model)
+    return model
 
-    # оборачиваем модель в PEFT
-    model = get_peft_model(model, lora_config)
+
+def load_model():
+    """
+    Для CPT создает модель с пустым LoRA-адаптером
+    """
+    base_model = load_base_quantized_model()
+
+    lora_config = LoraConfig(
+        r=LORA_R,
+        lora_alpha=LORA_ALPHA,
+        lora_dropout=LORA_DROPOUT,
+        bias="none",
+        task_type="CAUSAL_LM",
+        target_modules="all-linear"
+    )
+
+    model = get_peft_model(base_model, lora_config)
     model.print_trainable_parameters()
-
     return model
