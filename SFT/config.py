d31bdaa875c9064586268ffa9c2adb9c16ae2929
RedWolf77 <romario270704@mail.ru>
2026-06-24 09:23:33 +0200

    refactor: merge two config file into one

--- SFT/config.py
+++
@@ -1,8 +0,0 @@
-SFT_DATASET = "data/train.jsonl"
-SFT_OUTPUT_DIR = "output/qwen_sft"
-
-SFT_MAX_LENGTH = 2048       # максимальная длина в токенах
-
-SFT_LEARNING_RATE = 1e-5    # шаг градиентного спуска
-
-SFT_EPOCHS = 2              # колчество эпох
