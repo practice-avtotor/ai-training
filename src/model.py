79d5467c52331dedef74c2590f73dca7906cc222
RedWolf77 <romario270704@mail.ru>
2026-06-29 14:49:04 +0200

    feat: add optimization for low memory

--- src/model.py
+++ src/model.py
@@ -13,19 +13,20 @@ def load_base_quantized_model():
     bnb_config = BitsAndBytesConfig(
         load_in_4bit=True,
         bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.bfloat16
+        bnb_4bit_compute_dtype=torch.bfloat16,
+        bnb_4bit_use_double_quant=True
     )
 
     # Загрузка модели
     model = AutoModelForCausalLM.from_pretrained(
         MODEL_NAME,
         quantization_config=bnb_config,
-        device_map="auto",
+        device_map={"": 0},
         trust_remote_code=True
     )
 
     # Подготавливаем модель для kbit обучения
-    model = prepare_model_for_kbit_training(model)
+    model = prepare_model_for_kbit_training(model, use_gradient_checkpointing=True)
     return model
 
 
