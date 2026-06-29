d08ed8f7009fce6df72aeb75ca5fa3533a180a41
RedWolf77 <romario270704@mail.ru>
2026-06-29 15:05:43 +0200

    feat: add cache cleaning

--- train_sft.py
+++ train_sft.py
@@ -3,9 +3,14 @@ from src.dataset import load_sft_dataset
 from src.sft_trainer import create_sft_trainer
 from src.model import load_cpt_model, load_model
 from src.tokenizer import load_tokenizer
-
+import gc
+import torch
 
 def main():
+    # Очищаем VRAM от остатков прошлых падений скрипта
+    gc.collect()
+    torch.cuda.empty_cache()
+
     print("Загрузка датасета...")
 
     dataset = load_sft_dataset(SFT_DATASET)
